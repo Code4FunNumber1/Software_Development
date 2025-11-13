f39918eb95e85d9d54c60544853cd2a20a61a7dc
stephjac000 <stephjac000@warren.k12.in.us>
2025-11-13 13:49:15 -0500

    Yet another code along...yay.

--- Exceptions/EX-CodeAlong1/src/TryCatchDemo.java
+++ Exceptions/EX-CodeAlong1/src/TryCatchDemo.java
@@ -1,5 +1,15 @@
 public class TryCatchDemo {
     public static void main(String[] args) {
 
+        try {
+            int a = 10;
+            int b = 0;
+            int result = a / b;
+            System.out.println("Result = %s".formatted(result));
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("finally");
+        }
     }
 }
---
+++ Exceptions/EX-CodeAlong2/src/AgeValidator.java
@@ -0,0 +1,26 @@
+public class AgeValidator {
+
+    public static void main(String[] args) {
+        try {
+            int testAge = 15;
+            validateAge(testAge);
+            System.out.printf("Age %s is valid%n", testAge);
+        } catch (InvalidAgeException e) {
+            System.out.printf("Validation Failed: %s%n",e.getMessage());
+        } catch (Exception e) {
+            System.out.printf("Exception Occurred: %s%n",e.getMessage());
+        } finally {
+            System.out.println("Finished Age Validation Attempt.");
+        }
+    }
+
+    public static void validateAge(int age) throws InvalidAgeException {
+        if (age < 0 || age > 130) {
+            throw new InvalidAgeException("Age %s is not realistic.".formatted(age));
+        }
+
+        if (age < 18) {
+            throw new InvalidAgeException("Age %s is too young.".formatted(age));
+        }
+    }
+}
